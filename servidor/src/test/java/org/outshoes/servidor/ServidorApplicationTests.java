d9d8d30934a22cc05e0fd92c59282c2f32425777
Tyron Vergara <tyron.vergara@gmail.com>
2022-07-07 22:44:14 -0500

    feat: Se añaden los archivos principales para el proyecto
    Se añaden los archivos necesarios para comenzar la tarea

---
+++ servidor/src/test/java/org/outshoes/servidor/ServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.outshoes.servidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
